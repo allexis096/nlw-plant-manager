e71d6018087b3d450d3f4f7bab49cfa38cf3cd19
allexis096 <allexissoares@gmail.com>
2021-04-21 11:53:04 -0300

    :zap: chore: upgrade dependencies

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -4,6 +4,7 @@ import colors from '../styles/colors';
 import { Welcome } from '../screens/Welcome';
 import { UserIdentification } from '../screens/UserIdentification';
 import { Confirmation } from '../screens/Confirmation';
+import { PlantSelect } from '../screens/PlantSelect';
 
 const stackRoutes = createStackNavigator();
 
@@ -22,6 +23,7 @@ const AppRoutes: React.FC = () => (
       component={UserIdentification}
     />
     <stackRoutes.Screen name="Confirmation" component={Confirmation} />
+    <stackRoutes.Screen name="PlantSelect" component={PlantSelect} />
   </stackRoutes.Navigator>
 );
 
